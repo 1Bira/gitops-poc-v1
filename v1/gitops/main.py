4a0add9784d1a0846d222eb7a09f707968155954
1bira <ubiratan.gleidson@gmail.com>
2024-05-27 12:27:16 +0100

    add gitops python scripts

---
+++ v1/gitops/main.py
@@ -0,0 +1,22 @@
+import os
+from github import Github
+from github import Auth
+
+#
+# To test use # export REPO_KEY=??
+#
+
+def main():
+    github_key = os.environ["REPO_KEY"]
+
+    auth = Auth.Token(github_key)
+    
+    #g = Github(auth=auth)
+
+    g = Github(base_url="https://github.com/1Bira/gitops-repo-poc-v1.git", auth=auth)
+    g.get_user().login
+
+    
+
+if __name__ == "__main__":
+    main()
