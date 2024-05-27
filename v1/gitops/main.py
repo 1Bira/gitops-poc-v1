f3178cd27b59bb65e2cc622d97a4fa1d0f60b9b0
1Bira <ubiratan.gleidson@gmail.com>
2024-05-27 14:46:32 +0100

    Update main.py

--- v1/gitops/main.py
+++ v1/gitops/main.py
@@ -14,7 +14,21 @@ def main():
     g = Github(auth=auth)
 
     #g = Github(base_url="https://github.com/1Bira/", auth=auth)
-    print(g.get_repo("1Bira/gitops-repo-poc-v1").name)
+    repo = g.get_repo("1Bira/gitops-repo-poc-v1")
+    repo.get_branch(branch="main")
+    all_files = []
+    contents = repo.get_contents("")
+
+    while contents:
+        file_content = contents.pop(0)
+        if file_content.type == "dir":
+            contents.extend(repo.get_contents(file_content.path))
+        else:
+            file = file_content
+            all_files.append(str(file).replace('ContentFile(path="', '').replace('")', ''))
+    for file in allfiles:
+        print(file)
+
 
     
 
